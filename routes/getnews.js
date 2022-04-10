3d0439c3d6bdc226a4b69a9c5c80aa147fc98551
Prasoon Soni <prasoonsoni.work@gmail.com>
2022-04-10 23:50:31 +0530

    Updates

--- routes/getnews.js
+++ routes/getnews.js
@@ -13,13 +13,24 @@ router.get('/', async (req, res) => {
         var success = true;
         const contract = await new web3.eth.Contract(contractABI, contractAddress);
         const news = await contract.methods.getAllNews().call();
+        const newses = []
         for(let i = 0; i < news.length; i++) {
             console.log(news[i]);
+            newses.push({
+                uploader_address: news[i].uploader_address,
+                title: news[i].title,
+                description: news[i].description,
+                publisher: news[i].publisher,
+                date_published: news[i].date_published,
+                block_timestamp: news[i].block_timestamp,
+                block_number: news[i].block_number,
+                block_data: news[i].block_data
+            });
         }
         if(!news){
             success = false;
         }
-        res.send({success, news});
+        res.send({success, news:newses});
     } catch (error) {
         console.log(error);
     }
